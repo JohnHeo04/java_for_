543ebf51a1aa090a0bf8bfb47de47accf291c21f
JohnHeo04 <47841046+JohnHeo04@users.noreply.github.com>
2020-04-12 17:33:13 +0900

    Add files via upload
    More clearly!

--- week4_scanner_class.java
+++ week4_scanner_class.java
@@ -8,24 +8,25 @@ import java.util.Scanner;
 }
 */
 
-
 public class week4_scanner_class extends LoginDemo_0 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.print("정수값(n)을 입력하세요.");
 		double n = scanner.nextInt();
-		double i = 1;
+		double n1 = 1;
 		double result = 0;
-		
-		for(double n1 = 1; n1 <= n; n1++) {
+
+		for (double i = 1; i <= n; i++) {
 //			System.out.println(i/n1);
-			result += i/n1;
+			result += n1 / i;
 		}
+
 		System.out.println(result);
-		
-		
+
 	}
 
 }
+
+
