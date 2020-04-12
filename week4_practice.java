323eb20d403b3489f09f16bf4f6e549e6298ceb5
JohnHeo04 <47841046+JohnHeo04@users.noreply.github.com>
2020-04-12 20:43:33 +0900

    Add files via upload

--- week4_practice.java
+++ week4_practice.java
@@ -7,15 +7,15 @@ public class week4_practice extends LoginDemo_0 {
 		//3개의 정수를 입력받아서 가장 큰 값을 출력. 단 삼항조건연산자를 사용하시오.
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("5개의 정수를 공백으로 띄워서 입력하세요!");
-		double i1 = scanner.nextDouble();
-		double i2 = scanner.nextDouble();
-		double i3 = scanner.nextDouble();
-		double i4 = scanner.nextDouble();
-		double i5 = scanner.nextDouble();
+		int i1 = scanner.nextInt();
+		int i2 = scanner.nextInt();
+		int i3 = scanner.nextInt();
+		int i4 = scanner.nextInt();
+		int i5 = scanner.nextInt();
 	
 		
 //		3항 연산자를 2번 써서 나타내기 		
-		double max = ((i1>i2) ? i1:i2);
+		int max = ((i1>i2) ? i1:i2);
 		max = ((max>i3) ? max:i3);
 		max = ((max>i4) ? max:i4);
 		max = ((max>i5) ? max:i5);
