3e0d012cba8b882438c6cf95e4b3be5358bd7b26
JohnHeo04 <47841046+JohnHeo04@users.noreply.github.com>
2020-04-20 06:41:30 +0900

    Add files via upload

---
+++ week5_multi.java
@@ -0,0 +1,27 @@
+
+public class week5_multi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 1; i<10; i++) {
+			for(int j = 2; j<6; j++) {
+				System.out.print(j + " * " + i + " = " + i*j + ",");
+				System.out.print('\t');
+				
+				}
+			System.out.println();
+		}
+			System.out.println();
+		for(int k = 1; k<10; k++) {
+			for(int l = 6; l<10; l++) {
+				System.out.print(l + " * " + k + " = " + k*l + ",");
+				System.out.print('\t');
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
